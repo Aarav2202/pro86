eb449f23d98b2b4e53e2645cee4ac9856dba7161
Aarav Goel <mohitgoel13111980@gmail.com>
2022-03-31 17:44:21 +0530

    pro84

--- screens/Feed.js
+++ screens/Feed.js
@@ -21,10 +21,8 @@ export default class Feed extends Component {
         this.state = {};
     }
 
-    componentDidMount() { }
-
     renderItem = ({ item: post }) => {
-        return <PostCard post={post} />;
+        return <PostCard post={post} navigation = {this.props.navigation}/>;
     };
 
     keyExtractor = (item, index) => index.toString();
