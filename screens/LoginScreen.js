2f60dcb91fc27f7ed8087a6526b552ac84e32a01
Aarav Goel <mohitgoel13111980@gmail.com>
2022-03-31 18:00:38 +0530

    pro86

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Button } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image,
+    TouchableOpacity
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
 import * as Google from "expo-google-app-auth";
 import firebase from "firebase";
 
 export default class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+
+    }
+
     isUserEqual = (googleUser, firebaseUser) => {
         if (firebaseUser) {
             var providerData = firebaseUser.providerData;
@@ -95,10 +115,26 @@ export default class LoginScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Button
-                    title="Sign in with Google"
-                    onPress={() => this.signInWithGoogleAsync()}
-                ></Button>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={styles.appTitle}>
+                    <Image
+                        source={require("../assets/logo.png")}
+                        style={styles.appIcon}
+                    ></Image>
+                    <Text style={styles.appTitleText}>Spectagram</Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => this.signInWithGoogleAsync()}
+                    >
+                        <Image
+                            source={require("../assets/google_icon.png")}
+                            style={styles.googleIcon}
+                        ></Image>
+                        <Text style={styles.googleText}>Sign in with Google</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -107,7 +143,47 @@ export default class LoginScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.4,
         justifyContent: "center",
         alignItems: "center"
+    },
+    appIcon: {
+        width: RFValue(130),
+        height: RFValue(130),
+        resizeMode: "contain"
+    },
+    appTitleText: {
+        color: "white",
+        textAlign: "center",
+        fontSize: RFValue(40)
+    },
+    buttonContainer: {
+        flex: 0.3,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    button: {
+        width: RFValue(250),
+        height: RFValue(50),
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        borderRadius: RFValue(30),
+        backgroundColor: "white"
+    },
+    googleIcon: {
+        width: RFValue(30),
+        height: RFValue(30),
+        resizeMode: "contain"
+    },
+    googleText: {
+        color: "black",
+        fontSize: RFValue(20)
     }
 });
