e9585a08be3e48cba9d6dad71e8781627e5b7389
Aarav Goel <mohitgoel13111980@gmail.com>
2022-03-31 17:56:09 +0530

    pro85

---
+++ config.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDD1rn_uqQhLxUYepv7L51ODdD8XJX9qbg",
+    authDomain: "spectagram-827e6.firebaseapp.com",
+    projectId: "spectagram-827e6",
+    storageBucket: "spectagram-827e6.appspot.com",
+    messagingSenderId: "151367911195",
+    appId: "1:151367911195:web:408504a0b0cb0e1b33b5be"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
